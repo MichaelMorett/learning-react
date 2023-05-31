9d80877eb13cad3c47838494473b4e7bdbbab841
michaelmorett <michaelmorett@yahoo.com>
2023-05-30 18:30:01 -0700

    ExpandableText component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product 1", "Product 2"]);
-
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart
-        cartItems={cartItems}
-        onClear={() => {
-          setCartItems([]);
-        }}
-      />
+      <ExpandableText maxChars={25}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+        tenetur laborum veniam corporis at. Porro similique aut accusantium quos
+        soluta eaque itaque maiores recusandae provident, temporibus eveniet
+        doloribus! Est dolorem quia vitae debitis magni accusamus expedita
+        aliquam asperiores, temporibus officiis ducimus. Exercitationem odit
+        omnis blanditiis accusantium ipsa harum, eligendi magni expedita
+        similique amet nostrum, laudantium eum. Repudiandae harum, blanditiis
+        dolor illo necessitatibus voluptatibus minima sunt rem consequatur vitae
+        amet. Dolorum explicabo delectus qui! Incidunt blanditiis numquam, illo
+        reiciendis modi sit facilis officia exercitationem quidem temporibus
+        eveniet. Mollitia harum delectus rem unde, quia nam quidem ut deserunt
+        officia dolorem ratione itaque?
+      </ExpandableText>
     </div>
   );
 }
