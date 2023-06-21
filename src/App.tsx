3defa717bd6e6fe8b9b71809c59b09836583077c
michaelmorett <michaelmorett@yahoo.com>
2023-06-21 12:54:28 -0700

    Using useRef to reference input fields.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <div>
-      <ExpandableText maxChars={25}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-        tenetur laborum veniam corporis at. Porro similique aut accusantium quos
-        soluta eaque itaque maiores recusandae provident, temporibus eveniet
-        doloribus! Est dolorem quia vitae debitis magni accusamus expedita
-        aliquam asperiores, temporibus officiis ducimus. Exercitationem odit
-        omnis blanditiis accusantium ipsa harum, eligendi magni expedita
-        similique amet nostrum, laudantium eum. Repudiandae harum, blanditiis
-        dolor illo necessitatibus voluptatibus minima sunt rem consequatur vitae
-        amet. Dolorum explicabo delectus qui! Incidunt blanditiis numquam, illo
-        reiciendis modi sit facilis officia exercitationem quidem temporibus
-        eveniet. Mollitia harum delectus rem unde, quia nam quidem ut deserunt
-        officia dolorem ratione itaque?
-      </ExpandableText>
-    </div>
-  );
+  return <Form />;
 }
 
 export default App;
